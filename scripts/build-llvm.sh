734b9d0fc4df240510806264305bc66d6f6e4a9e
Rajendra S Y <rajendra.sy@gmail.com>
2024-03-07 13:41:35 +0530

    Updated

--- scripts/build-llvm.sh
+++ scripts/build-llvm.sh
@@ -6,10 +6,24 @@ MACHINE=`uname -m`
 TOP=$PWD
 TODAY=`date +%Y%m%d`
 BUILD=$TOP/build/$TODAY
+rm -rf $BUILD
+mkdir -p $BUILD
+
 cmake -S $TOP/llvm-project/llvm -B $BUILD \
         -DCMAKE_BUILD_TYPE=Release \
         -DLLVM_ENABLE_PROJECTS=all \
+	-DLLVM_ENABLE_RUNTIMES=all \
+	-DLLVM_TARGETS_TO_BUILD=all \
+	-DLLVM_ENABLE_RTTI=True \
         -DCMAKE_CXX_COMPILER=clang++ \
-        -DCMAKE_C_COMPILER=clang
+        -DCMAKE_C_COMPILER=clang \
+	-DLLVM_USE_LINKER=lld \
+	-DLLVM_PARALLEL_LINK_JOBS=1 \
+	-DLLVM_PARALLEL_COMPILE_JOBS=4 \
+	-DCLANG_DEFAULT_CXX_STDLIB=libc++ \
+	-DSANITIZER_CXX_ABI=libc++ \
+	-DLLVM_ENABLE_Z3_SOLVER=OFF
+
 cmake --build $BUILD -j4
 cmake --install $BUILD --prefix /volume/hab/$OS/$OS_ID/$MACHINE/llvm/main/$TODAY
+
